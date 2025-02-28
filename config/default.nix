6f2d1c4a2579bfaac3cc62b7597200c58bc1b7c1
mj <28581723+jee-mj@users.noreply.github.com>
2025-02-28 17:16:15 +1100

    Update default.nix

--- config/default.nix
+++ config/default.nix
@@ -27,12 +27,12 @@
   bufferlines.enable = lib.mkDefault true;
   colorschemes.enable = lib.mkDefault true;
   completion.enable = lib.mkDefault true;
-  dap.enable = lib.mkDefault true;
+  dap.enable = lib.mkDefault false;
   filetrees.enable = lib.mkDefault false;
   git.enable = lib.mkDefault true;
   keys.enable = true;
-  languages.enable = true;
-  lsp.enable = lib.mkDefault true;
+  languages.enable = false;
+  lsp.enable = lib.mkDefault false;
   none-ls.enable = lib.mkDefault false;
   sets.enable = lib.mkDefault true;
   pluginmanagers.enable = lib.mkDefault true;
